5032ae983eefd98133c7b24040bab2d3080c5d29
Luca <lucariossantillan.trabajo@gmail.com>
2026-06-21 20:48:49 -0300

    Fix: Sincronizar credenciales de base de datos y agregar puerto para prueba de host

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ export const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,  // 2. Agregado para que lea el puerto 18535 en la nube
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
