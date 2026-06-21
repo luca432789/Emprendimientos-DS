1da6978181650e181d37aace90d17f18fec25220
Luca <lucariossantillan.trabajo@gmail.com>
2026-06-21 19:32:36 -0300

    Fix: Estructurar app.js con variables de entorno y middleware estático

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Configuracion inicial del sistema
 import 'dotenv/config'; // 👈 ESTA LÍNEA DEBE SER LA PRIMERA DEL ARCHIVO
 console.log("REVISANDO CLAVE JWT:", process.env.JWT_SECRET);
 
+// Librerias del sistema
 import express from 'express';
 import cors from 'cors';
 
@@ -8,20 +10,21 @@ import cors from 'cors';
 import etiquetasRouter from './routes/etiquetasRoutes.js';
 import loginRouter from './routes/loginRoutes.js';
 
-
+// inicializamos la app express
 const app = express();
-const PORT = 3000;
 
-// Middlewares globales
+// Middlewares de control (CORS y procesamiento de JSON)
+app.use(cors()); 
 app.use(express.json());
-app.use(cors());
+
 app.use(express.static('public')); // Servirá tus HTMLs, CSS e Imágenes automáticos
 
-// Conectamos las rutas al servidor web
+// Configuracion de las rutas
 app.use('/api', etiquetasRouter); // Todo lo de etiquetas colgará de /api/etiquetas
 app.use('/api', loginRouter);      // Todo lo de login colgará de /api/login
 
 // Encendido del servidor
+const PORT = process.env.PORT ||3000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor API corriendo en http://localhost:${PORT}`);
 });
