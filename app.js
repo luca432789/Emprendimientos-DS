7c4c0e35ba9f9d336a5cff89b871aad8695061e6
Victorjoaquinibanez <victorjoaquinibanez@gmail.com>
2026-07-02 03:08:56 -0300

    feat(mapa): integrar vista de mapa y navegación desde index

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ console.log("REVISANDO CLAVE JWT:", process.env.JWT_SECRET);
 // Librerias del sistema
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Importamos los enrutadores externos
 import etiquetasRouter from './routes/etiquetasRoutes.js';
 import loginRouter from './routes/loginRoutes.js';
+import mapaRouter from './routes/mapaRoutes.js';
 
 // inicializamos la app express
 const app = express();
@@ -17,11 +23,17 @@ const app = express();
 app.use(cors()); 
 app.use(express.json());
 
-app.use(express.static('public')); // Servirá tus HTMLs, CSS e Imágenes automáticos
+app.use(express.static(path.resolve('./public')));  // Servirá tus HTMLs, CSS e Imágenes automáticos
 
 // Configuracion de las rutas
 app.use('/api', etiquetasRouter); // Todo lo de etiquetas colgará de /api/etiquetas
 app.use('/api', loginRouter);      // Todo lo de login colgará de /api/login
+app.use('/api', mapaRouter);       // Todo lo de mapa colgará de /api/emprendimientos mapa
+
+app.get('/mapa', (req, res) => {
+    console.log("ENTRÉ A /MAPA"); // 👈 DEBUG
+    res.sendFile(path.join(__dirname, 'public', 'mapa.html'));
+});
 
 // Encendido del servidor
 const PORT = process.env.PORT ||3000;
